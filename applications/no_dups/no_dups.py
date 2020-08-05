2d3c2d0b691020148420ba15c693c4766435f35f
johnkirtley <kirtleyj16@gmail.com>
2020-08-05 15:47:22 -0700

    Completed No Dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,17 @@
 def no_dups(s):
     # Your code here
+    cache = {}
+    arr = s.split(" ")
+    newString = ""
 
+    for val in arr:
+        if val not in cache:
+            cache[val] = val
+
+    for val in cache:
+        newString += f" {val}"
+
+    return newString.strip()
 
 
 if __name__ == "__main__":
